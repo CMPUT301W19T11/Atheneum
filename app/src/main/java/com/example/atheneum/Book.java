85cea16d4a43bfca863582637388f82a280472f3
Marcus Boay <marcusboay@hotmail.com>
2019-02-20 12:58:39 -0700

    add Book class with attributes, gettters and setters

---
+++ app/src/main/java/com/example/atheneum/Book.java
@@ -0,0 +1,105 @@
+package com.example.atheneum;
+
+import java.util.List;
+import java.util.UUID;
+
+public class Book {
+    private int isbn;
+    private String title;
+    private String description;
+    private String author;
+    private User owner;
+    private User borrower;
+    private Status status;
+    private List<Request> requests; //Request represents our Request class. NOT java's!
+    private UUID bookID;
+    private List<String> photos;
+
+    public Book() {
+        this.bookID = UUID.randomUUID();
+    }
+
+    public int getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public User getOwner() {
+        return owner;
+    }
+
+    public void setOwner(User owner) {
+        this.owner = owner;
+    }
+
+    public User getBorrower() {
+        return borrower;
+    }
+
+    public void setBorrower(User borrower) {
+        this.borrower = borrower;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Request> getRequests() {
+        return requests;
+    }
+
+    public void setRequests(List<Request> requests) {
+        this.requests = requests;
+    }
+
+    public void addRequest(Request request) {
+        this.requests.add(request);
+    }
+
+    public UUID getBookID() {
+        return bookID;
+    }
+
+    public List<String> getPhotos() {
+        return photos;
+    }
+
+    public void setPhotos(List<String> photos) {
+        this.photos = photos;
+    }
+
+    public void addPhoto(String photo) {
+        this.photos.add(photo);
+    }
+}
