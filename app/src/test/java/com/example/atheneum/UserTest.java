aa4a1993bc85945b7518b3a292b703ed0a9017bc
Kaho-Lee <ljh243771593@hotmail.com>
2019-02-20 21:54:52 -0700

    part3 user class without notification

---
+++ app/src/test/java/com/example/atheneum/UserTest.java
@@ -0,0 +1,150 @@
+/*
+ * Copyright <YEAR> <COPYRIGHT HOLDER>
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ */
+
+package com.example.atheneum;
+
+import org.junit.Before;
+import org.junit.Test;
+import java.util.ArrayList;
+import static org.junit.Assert.*;
+
+public class UserTest {
+    private User user1;
+    private ArrayList<String> testListOwning;
+    private ArrayList<String> testListBorrowing;
+    private ArrayList<String> testListPhotos;
+
+    @Before
+    public void init(){
+        user1 = new User("user1@ualberta.ca");
+        testListOwning = new ArrayList<String>();
+        testListBorrowing = new ArrayList<String>();
+        testListPhotos = new ArrayList<String>();
+    }
+
+    @Test
+    public void checkUserName(){
+        assertEquals("user1@ualberta.ca", user1.getUserName());
+    }
+
+    @Test
+    public void getPhoneNumber(){
+        int phoneNumber = 1234;
+        user1.setPhoneNumber(phoneNumber);
+        assertEquals(phoneNumber, user1.getPhoneNumber());
+    }
+
+    @Test
+    public void getOwnerRate(){
+        double rate = 10.0;
+        double delta = 0.000000001;
+        user1.setOwnerRate(rate);
+        assertEquals(rate, user1.getOwnerRate(), delta);
+    }
+
+    @Test
+    public void getBorrowerRate(){
+        double rate = 10.0;
+        double delta = 0.000000001;
+        user1.setBorrowerRate(rate);
+        assertEquals(rate, user1.getBorrowerRate(), delta);
+    }
+
+    @Test
+    public void getOwning(){
+
+        testListOwning.add("book1");
+        testListOwning.add("book2");
+        testListOwning.add("book3");
+
+        user1.setOwning(testListOwning);
+        assertTrue(testListOwning.equals(user1.getOwning()));
+    }
+
+    @Test
+    public void getBorrowing(){
+
+        testListBorrowing.add("book1");
+        testListBorrowing.add("book2");
+        testListBorrowing.add("book3");
+
+        user1.setBorrowing(testListBorrowing);
+        assertTrue(testListBorrowing.equals(user1.getBorrowing()));
+    }
+
+    @Test
+    public void getPhotos(){
+
+        testListPhotos.add("photo1");
+        testListPhotos.add("photo2");
+        testListPhotos.add("photo3");
+
+        user1.setPhotos(testListPhotos);
+        assertTrue(testListPhotos.equals(user1.getPhotos()));
+    }
+
+    @Test
+    public void addOwning(){
+
+        String newbook = "book4";
+        testListOwning.add(newbook);
+        user1.addOwning(newbook);
+
+        assertTrue(testListOwning.equals(user1.getOwning()));
+    }
+
+    @Test
+    public void deleteOwning(){
+        String newbook = "book4";
+        testListOwning.remove(newbook);
+        user1.deleteOwing(newbook);
+
+        assertTrue(testListOwning.equals(user1.getOwning()));
+    }
+
+    @Test
+    public void addBorrowing(){
+        String newbook = "book4";
+        testListBorrowing.add(newbook);
+        user1.addBorrowing(newbook);
+
+        assertTrue(testListBorrowing.equals(user1.getBorrowing()));
+    }
+
+    @Test
+    public void deleteBorrowing(){
+        String newbook = "book4";
+        testListBorrowing.remove(newbook);
+        user1.deleteBorrowing(newbook);
+
+        assertTrue(testListBorrowing.equals(user1.getBorrowing()));
+    }
+
+    @Test
+    public void addPhoto(){
+        String newPhoto = "photo4";
+        testListPhotos.add(newPhoto);
+        user1.addPhotos(newPhoto);
+
+        assertTrue(testListPhotos.equals(user1.getPhotos()));
+    }
+
+    @Test
+    public void deletePhotos(){
+        String newPhoto = "photo4";
+        testListPhotos.remove(newPhoto);
+        user1.deletePhotos(newPhoto);
+
+        assertTrue(testListPhotos.equals(user1.getPhotos()));
+    }
+
+
+
+}
