dbb866bcc2cb1f6349874b1ad51d802807cca169
Kaho-Lee <ljh243771593@hotmail.com>
2019-02-23 11:19:11 -0700

    user class at 0223

--- app/src/main/java/com/example/atheneum/User.java
+++ app/src/main/java/com/example/atheneum/User.java
@@ -24,7 +24,9 @@ public class User {
     private ArrayList<String> borrowing;
     private ArrayList<String> photos;
 
+    public User(){
 
+    }
 
     public User(String userName, String phoneNumber, double ownerRate, double borrowerRate){
         this.userName = userName;
