b384e7f0cd33aacf02f9bede6d47640c4a701f0d
Kaho-Lee <ljh243771593@hotmail.com>
2019-02-23 21:05:48 -0700

    user class and OwnerCollection class

--- app/src/main/java/com/example/atheneum/OwnerCollection.java
+++ app/src/main/java/com/example/atheneum/OwnerCollection.java
@@ -6,30 +6,52 @@ public class OwnerCollection {
     private User owner;
     private ArrayList<Book> ownList;
 
-
+    /**
+     * blank constructor
+     */
     public OwnerCollection(){
         this.owner = new User();
         this.ownList = new ArrayList<Book>();
     }
 
-
+    /**
+     * constructor with specified attributes
+     * @param owner
+     */
     public OwnerCollection(User owner){
         this.owner = owner;
         this.ownList = new ArrayList<Book>();
     }
 
-    public void setOwner(User owner){this.owner = owner;}
-
-    public void setOwnList(ArrayList<Book> ownList){this.ownList = ownList;}
-
+    /**
+     *
+     * @return owner of this collection
+     */
     public User getOwner(){return this.owner;}
 
+    /**
+     *
+     * @return collection of the owning book
+     */
     public ArrayList<Book> getOwnList(){return this.ownList;}
 
+    /**
+     * add new owning book
+     * @param book
+     */
     public void addBook(Book book){this.ownList.add(book);}
 
+    /**
+     * delete a owning book
+     * @param book
+     */
     public void deleteBook(Book book){this.ownList.remove(book);}
 
+    /**
+     * filter out a book list by status
+     * @param status
+     * @return a book list with specified status
+     */
     public ArrayList<Book> filterBooks(Book.Status status){
         ArrayList<Book> filteredList = new ArrayList<Book>();
 
