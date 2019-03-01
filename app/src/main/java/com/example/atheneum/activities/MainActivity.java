e832c3eb2a009d47356883b862e6223bd8516b64
Ishaat Chowdhury <ishaat@ualberta.ca>
2019-02-28 22:41:10 -0700

    Rename BaseActivity to MainActivity
    Renames BaseActivity to MainActivity and removes abstract
    class. This is done in preparation for a refactor of using
    fragments.

--- app/src/main/java/com/example/atheneum/activities/MainActivity.java
+++ app/src/main/java/com/example/atheneum/activities/MainActivity.java
@@ -1,22 +1,98 @@
 package com.example.atheneum.activities;
 
-import android.support.constraint.ConstraintLayout;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.atheneum.R;
+import com.firebase.ui.auth.AuthUI;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
-
-/**
- * Default Activity after User sign's in.
- */
-public class MainActivity extends BaseActivity {
+// See: https://stackoverflow.com/a/36103112/11039833
+// See: https://stackoverflow.com/a/19451842/11039833
+public class MainActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener {
+    private String TAG = MainActivity.class.getSimpleName();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Remember this is the Constraint Layout area within content_base.xml
-        ConstraintLayout contentFrameLayout = (ConstraintLayout) findViewById(R.id.content_frame);
-        getLayoutInflater().inflate(R.layout.content_main, contentFrameLayout);
-        this.setTitle(R.string.title_activity_main);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        View headerView = navigationView.getHeaderView(0);
+        TextView nav_username = (TextView)((View) headerView).findViewById(R.id.nav_user_name);
+        FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+        if (firebaseUser != null) {
+            // Set the user's email on the nav bar
+            nav_username.setText(firebaseUser.getEmail());
+        } else {
+            Log.w(TAG, "User should be authenticated if the user is in this activity!");
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_home) {
+            // Do nothing!
+        } else if (id == R.id.nav_profile) {
+
+        } else if (id == R.id.nav_owner) {
+
+        } else if (id == R.id.nav_borrower) {
+
+        } else if (id == R.id.nav_logout) {
+            // Sign out of account and go back to authentication screen
+            AuthUI.getInstance()
+                    .signOut(this)
+                    .addOnCompleteListener(new OnCompleteListener<Void>() {
+                        public void onComplete(@NonNull Task<Void> task) {
+                            // ...
+                            Intent intent = new Intent(getApplicationContext(), FirebaseUIAuthActivity.class);
+                            startActivity(intent);
+                        }
+                    });
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
