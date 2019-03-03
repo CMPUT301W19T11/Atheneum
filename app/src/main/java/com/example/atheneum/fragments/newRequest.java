449715e07e5e52f33e514c7ba31647c827b8dcec
Kaho-Lee <ljh243771593@hotmail.com>
2019-03-02 23:35:31 -0700

    add a new request with adding book UUID manually

--- app/src/main/java/com/example/atheneum/fragments/newRequest.java
+++ app/src/main/java/com/example/atheneum/fragments/newRequest.java
@@ -2,22 +2,40 @@ package com.example.atheneum.fragments;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 
 import com.example.atheneum.R;
 import com.example.atheneum.activities.MainActivity;
+import com.example.atheneum.models.Request;
+import com.example.atheneum.models.User;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.UUID;
 
 public class newRequest extends Fragment {
+
+    private User requester;
+    private EditText bookIDTest;
     private View view;
     private MainActivity mainActivity = null;
     private Context context;
 
     private Button savebtn;
+    private static final String TAG = "AddRequest";
 
     public newRequest() {
         // required empty constructor
@@ -38,13 +56,45 @@ public class newRequest extends Fragment {
 
         }
 
+        bookIDTest = this.view.findViewById(R.id.bookIDTest);
+
         final FragmentManager fragmentManager = getFragmentManager();
         savebtn = this.view.findViewById(R.id.save);
         savebtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
+                final FirebaseDatabase db = FirebaseDatabase.getInstance();
+                DatabaseReference ref = db.getReference().child(getString(R.string.db_users)).child(currentUser.getUid());
+                ref.addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                        if (dataSnapshot.exists()) {
+                            requester = dataSnapshot.getValue(User.class);
+                            String bookID = bookIDTest.getText().toString();
+                            UUID bookUUID =UUID.fromString(bookID);
+                            Request newRequest = new Request(requester, bookUUID);
+                            // add book to the owner's collection
+                            Log.i(TAG, "send added, id=" + newRequest.getBookID().toString());
+                            DatabaseReference requestColref = db.getReference().child(getString(R.string.db_requestCollection)).child(newRequest.getRequester().getUserID());
+                            requestColref.child(newRequest.getBookID().toString()).setValue(newRequest);
+                            Log.i(TAG, "Request added, id=" + newRequest.getBookID().toString());
 
-                fragmentManager.beginTransaction().replace(R.id.content_frame, new BorrowerPageFragment()).commit();
+                            getActivity().getSupportFragmentManager().beginTransaction().remove(newRequest.this).commit();
+
+                            // return to owner page fragment
+                            getActivity().getSupportFragmentManager().beginTransaction().replace(R.id.content_frame, new OwnerPageFragment()).commit();
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError databaseError) {
+
+                    }
+                });
+
+//                fragmentManager.beginTransaction().replace(R.id.content_frame, new BorrowerPageFragment()).commit();
             }
         });
 
