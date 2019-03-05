a84fe5950d6f643e8f2cd5d807356d91dcf21c58
Alex Chen <yin6@ualberta.ca>
2019-03-05 11:46:43 -0700

    New util class for checking internet connectivity

---
+++ app/src/main/java/com/example/atheneum/utils/ConnectionChecker.java
@@ -0,0 +1,32 @@
+package com.example.atheneum.utils;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+
+/**
+ * Class for checking whether or not device is connected to the internet.
+ *
+ */
+public class ConnectionChecker {
+    // taken from https://stackoverflow.com/questions/14571478/using-google-books-api-in-android
+    private ConnectivityManager mConnectivityManager;
+    private Context context;
+
+    public ConnectionChecker(Context context) {
+        this.context = context;
+        // Instantiate mConnectivityManager
+        mConnectivityManager = (ConnectivityManager) context.getSystemService(Context.CONNECTIVITY_SERVICE);
+    }
+
+    public boolean isNetworkConnected(){
+        // Is device connected to the Internet?
+        NetworkInfo networkInfo = mConnectivityManager.getActiveNetworkInfo();
+        if(networkInfo != null && networkInfo.isConnected()){
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
