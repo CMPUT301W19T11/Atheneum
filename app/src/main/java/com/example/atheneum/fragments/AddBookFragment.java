abef93413a65fb50d0f01add46bc646ebfb5c8fe
Alex Chen <yin6@ualberta.ca>
2019-03-05 08:32:52 -0700

    Changedd API request to use SingletonReqestQueue

--- app/src/main/java/com/example/atheneum/fragments/AddBookFragment.java
+++ app/src/main/java/com/example/atheneum/fragments/AddBookFragment.java
@@ -26,6 +26,7 @@ import com.android.volley.toolbox.Volley;
 import com.example.atheneum.R;
 import com.example.atheneum.activities.MainActivity;
 import com.example.atheneum.models.Book;
+import com.example.atheneum.models.SingletonRequestQueue;
 import com.example.atheneum.models.User;
 import com.example.atheneum.utils.EditTextWithValidator;
 import com.example.atheneum.utils.NonEmptyTextValidator;
@@ -137,16 +138,13 @@ public class AddBookFragment extends Fragment {
             // auto populate with Google Books API
             String apiUrlString = "https://www.googleapis.com/books/v1/volumes?q=isbn:" + isbn_str;
 
-            // taken from https://developer.android.com/training/volley/simple.html
-            // Instantiate the RequestQueue.
-            RequestQueue queue = Volley.newRequestQueue(mainActivity);
-
+            // need final instances of these EditTexts to be usable inside the lambda below
             final EditText titleEditText = this.view.findViewById(R.id.bookTitleEditText);
             final EditText authorEditText = this.view.findViewById(R.id.authorEditText);
             final EditText descEditText = this.view.findViewById(R.id.descEditText);
 
+            // taken from https://developer.android.com/training/volley/request.html
             // Request a JSON response from the provided URL.
-
             JsonObjectRequest jsonObjectRequest = new JsonObjectRequest
                     (Request.Method.GET, apiUrlString, null, new Response.Listener<JSONObject>() {
 
@@ -185,7 +183,7 @@ public class AddBookFragment extends Fragment {
 
 
             // Add the request to the RequestQueue.
-            queue.add(jsonObjectRequest);
+            SingletonRequestQueue.getInstance(getContext()).addToRequestQueue(jsonObjectRequest);
 
         }
         else {
