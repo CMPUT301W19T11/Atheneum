dd3fe71840eaeeeb5161357277fa13e84121ff2b
Ishaat Chowdhury <ishaat@ualberta.ca>
2019-03-05 22:50:01 -0700

    Enable Offline Persistence

---
+++ app/src/main/java/com/example/atheneum/AtheneumFirebaseApp.java
@@ -0,0 +1,19 @@
+package com.example.atheneum;
+
+import com.google.firebase.database.FirebaseDatabase;
+
+/**
+ * Have to extend the base Android application class in order to enable disk persistence
+ *
+ * See: https://stackoverflow.com/a/37766261/11039833
+ */
+public class AtheneumFirebaseApp extends android.app.Application {
+
+        @Override
+        public void onCreate() {
+            super.onCreate();
+            /* Enable disk persistence  */
+            FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+            firebaseDatabase.setPersistenceEnabled(true);
+        }
+}
