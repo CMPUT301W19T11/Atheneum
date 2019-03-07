8ee7e3acd9b7b4acb602580ea7758fc3d342c766
Marcus Boay <marcusboay@hotmail.com>
2019-03-07 12:35:26 -0700

    put firebaseRecyclerAdapter stuff into BookInfoAcitivity for all requests on book

--- app/src/main/java/com/example/atheneum/activities/BookInfoActivity.java
+++ app/src/main/java/com/example/atheneum/activities/BookInfoActivity.java
@@ -15,22 +15,39 @@ import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModelProviders;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.DividerItemDecoration;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.atheneum.R;
 import com.example.atheneum.models.Book;
+import com.example.atheneum.utils.BookViewHolder;
+import com.example.atheneum.utils.FirebaseAuthUtils;
 import com.example.atheneum.viewmodels.BookInfoViewModel;
 import com.example.atheneum.viewmodels.BookInfoViewModelFactory;
+import com.example.atheneum.viewmodels.FirebaseRefUtils.BooksRefUtils;
 import com.example.atheneum.viewmodels.FirebaseRefUtils.DatabaseWriteHelper;
+import com.example.atheneum.viewmodels.FirebaseRefUtils.OwnerCollectionRefUtils;
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
 
 public class BookInfoActivity extends AppCompatActivity {
 
@@ -50,10 +67,14 @@ public class BookInfoActivity extends AppCompatActivity {
     private TextView textDesc;
     private TextView textStatus;
 
+    private RecyclerView requestsRecyclerView;
+    private FirebaseRecyclerAdapter firebaseRecyclerAdapter;
+    private RecyclerView.LayoutManager requestsLayoutManager;
+
     private Button deleteBtn;
     private Button editBtn;
 
-    private static final String TAG = "viewBookInfo";
+    private static final String TAG = BookInfoActivity.class.getSimpleName();
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -92,13 +113,98 @@ public class BookInfoActivity extends AppCompatActivity {
             }
         });
 
-        deleteBtn = findViewById(R.id.buttonDelete);
-        deleteBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                deleteBook();
-            }
-        });
+        //
+        if (FirebaseAuthUtils.isCurrentUserAuthenticated()) {
+            FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
+            Query keyQuery = OwnerCollectionRefUtils.getOwnerCollectionRef(firebaseUser.getUid());
+            DatabaseReference dataRef = BooksRefUtils.BOOKS_REF;
+
+            FirebaseRecyclerOptions<Book> options =
+                    new FirebaseRecyclerOptions.Builder<Book>()
+                            .setIndexedQuery(keyQuery, dataRef, Book.class)
+                            .build();
+
+            firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<Book, BookViewHolder>(options) {
+                @Override
+                protected void onBindViewHolder(@NonNull final BookViewHolder holder, int position, @NonNull final Book book) {
+                    //Bind Book object to BookViewHolder
+                    holder.titleTextView.setText(
+                            book.getTitle());
+                    holder.authorTextView.setText(
+                            book.getAuthor());
+                    holder.statusTextView.setText(
+                            book.getStatus().toString());
+//                    holder.bookItem.setOnClickListener(new View.OnClickListener() {
+//
+//                        @Override
+//                        public void onClick(View v) {
+//                            //                Toast.makeText(parent.getContext(), "Test Click" + String.valueOf(vh.getAdapterPosition()), Toast.LENGTH_SHORT).show();
+//                            Log.i("OwnerBook", "clicked on a book");
+//                            String sBookId = book.getBookID();
+//                            Intent intent = new Intent(context, BookInfoActivity.class);
+//                            intent.putExtra("bookID", sBookId);
+//                            intent.putExtra("position", holder.getAdapterPosition());
+//
+//                            mainActivity.startActivityForResult(intent, REQUEST_DELETE_ENTRY);
+//
+//                        }
+//                    });
+                }
+
+                @Override
+                public BookViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+                    // Create a new instance of the ViewHolder, in this case we are using a custom
+                    // layout called R.layout.message for each item
+                    // create a new view
+                    LinearLayout v = (LinearLayout) LayoutInflater.from(parent.getContext())
+                            .inflate(R.layout.book_card, parent, false);
+                    final BookViewHolder vh = new BookViewHolder(v);
+
+                    return vh;
+                }
+
+                @Override
+                public void onDataChanged() {
+                    // Called each time there is a new data snapshot. You may want to use this method
+                    // to hide a loading spinner or check for the "no documents" state and update your UI.
+                    // ...
+                }
+
+                @Override
+                public void onError(DatabaseError e) {
+                    // Called when there is an error getting data. You may want to update
+                    // your UI to display an error message to the user.
+                    // ...
+                    Log.i(TAG, e.getMessage());
+                }
+
+
+            };
+
+            ownerBooksRecyclerView = (RecyclerView) this.view.findViewById(R.id.owner_books_recycler_view);
+            ownerBooksRecyclerView.setHasFixedSize(true);
+            ownerBooksLayoutManager = new LinearLayoutManager(this.context);
+            ownerBooksRecyclerView.setLayoutManager(ownerBooksLayoutManager);
+            ownerBooksRecyclerView.setAdapter(firebaseRecyclerAdapter);
+            ownerBooksRecyclerView.addItemDecoration(new DividerItemDecoration(ownerBooksRecyclerView.getContext(),
+                    DividerItemDecoration.VERTICAL));
+
+            FloatingActionButton fab = (FloatingActionButton) this.view.findViewById(R.id.add_book);
+            fab.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Intent intent = new Intent(getActivity(), AddBookActivity.class);
+                    startActivity(intent);
+                }
+            });
+            deleteBtn = findViewById(R.id.buttonDelete);
+            deleteBtn.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    deleteBook();
+                }
+            });
+        }
         editBtn = findViewById(R.id.buttonEdit);
         //TODO: implement edit book button
     }
---
+++ app/src/main/java/com/example/atheneum/utils/BookRequestViewHolder.java
@@ -0,0 +1,4 @@
+package com.example.atheneum.utils;
+
+public class BookRequestViewHolder {
+}
