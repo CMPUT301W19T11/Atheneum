a41572e06b97d5fcb9840dedec9f7a9a8aa25ab9
Marcus Boay <marcusboay@hotmail.com>
2019-03-07 15:36:22 -0700

    register firebaseRecyclerAdapter in BookInfoActivity listeners

--- app/src/main/java/com/example/atheneum/activities/BookInfoActivity.java
+++ app/src/main/java/com/example/atheneum/activities/BookInfoActivity.java
@@ -42,10 +42,13 @@ import com.example.atheneum.viewmodels.BookInfoViewModelFactory;
 import com.example.atheneum.viewmodels.FirebaseRefUtils.BooksRefUtils;
 import com.example.atheneum.viewmodels.FirebaseRefUtils.DatabaseWriteHelper;
 import com.example.atheneum.viewmodels.FirebaseRefUtils.OwnerCollectionRefUtils;
+import com.example.atheneum.viewmodels.FirebaseRefUtils.RequestCollectionRefUtils;
 import com.firebase.ui.database.FirebaseRecyclerAdapter;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.firebase.ui.database.SnapshotParser;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
@@ -84,6 +87,18 @@ public class BookInfoActivity extends AppCompatActivity {
     }
 
     @Override
+    protected void onStart() {
+        super.onStart();
+        firebaseRecyclerAdapter.startListening();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        firebaseRecyclerAdapter.stopListening();
+    }
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_book_info);
@@ -118,12 +133,18 @@ public class BookInfoActivity extends AppCompatActivity {
         // get list of requesters
         if (FirebaseAuthUtils.isCurrentUserAuthenticated()) {
             FirebaseUser firebaseUser = FirebaseAuth.getInstance().getCurrentUser();
-            Query keyQuery = OwnerCollectionRefUtils.getOwnerCollectionRef(bookID);
-            DatabaseReference dataRef = BooksRefUtils.BOOKS_REF;
+            Query query = RequestCollectionRefUtils.getBookRequestCollectionRef(bookID);
 
             FirebaseRecyclerOptions<User> options =
                     new FirebaseRecyclerOptions.Builder<User>()
-                            .setIndexedQuery(keyQuery, dataRef, Book.class)
+                            .setQuery(query, new SnapshotParser<User>() {
+                                @NonNull
+                                @Override
+                                public User parseSnapshot(@NonNull DataSnapshot snapshot) {
+                                    Log.v(TAG, "PARSE SNAPSHOT METHOD IS BEING CALLED");
+                                    return new User();
+                                }
+                            })
                             .build();
 
             firebaseRecyclerAdapter = new FirebaseRecyclerAdapter<User, BookRequestViewHolder>(options) {
--- app/src/main/java/com/example/atheneum/viewmodels/FirebaseRefUtils/RequestCollectionRefUtils.java
+++ app/src/main/java/com/example/atheneum/viewmodels/FirebaseRefUtils/RequestCollectionRefUtils.java
@@ -3,6 +3,16 @@ package com.example.atheneum.viewmodels.FirebaseRefUtils;
 import com.google.firebase.database.DatabaseReference;
 
 public class RequestCollectionRefUtils extends RootRefUtils {
-    public static final DatabaseReference OWNER_REQUEST_COLLECTION_REF = ROOT_REF.child("requestCollection");
+    public static final DatabaseReference USER_REQUEST_COLLECTION_REF = ROOT_REF.child("requestCollection");
     public static final DatabaseReference BOOK_REQUEST_COLLECTION_REF = ROOT_REF.child("bookRequests");
+
+    //returns DatabaseReference of all books that the specified user has made
+    public static DatabaseReference getOwnerRequestCollectionRef(String userID) {
+        return USER_REQUEST_COLLECTION_REF.child(userID);
+    }
+
+    //return DatabaseReference of all users that made requests on specified book
+    public static DatabaseReference getBookRequestCollectionRef(String bookID) {
+        return BOOK_REQUEST_COLLECTION_REF.child(bookID);
+    }
 }
