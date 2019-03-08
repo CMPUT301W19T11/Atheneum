e0ba082b1b202129ba4479cfb41c060e36f6805b
Sam Wu <swm1000@hotmail.com>
2019-03-08 13:21:54 -0700

    remove navbar from view prof activity

--- app/src/main/java/com/example/atheneum/activities/ViewProfileActivity.java
+++ app/src/main/java/com/example/atheneum/activities/ViewProfileActivity.java
@@ -1,45 +1,22 @@
 package com.example.atheneum.activities;
 
-import android.arch.lifecycle.LiveData;
-import android.arch.lifecycle.Observer;
-import android.arch.lifecycle.ViewModelProviders;
-import android.content.Context;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.NavigationView;
-import android.support.v4.app.Fragment;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Base64;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.example.atheneum.R;
-import com.example.atheneum.activities.MainActivity;
-import com.example.atheneum.controllers.PictureController;
 import com.example.atheneum.fragments.EditProfileFragment;
-import com.example.atheneum.fragments.HomeFragment;
-import com.example.atheneum.fragments.ViewProfileFragment;
 import com.example.atheneum.models.User;
 import com.example.atheneum.utils.FirebaseAuthUtils;
-import com.example.atheneum.utils.PhotoUtils;
-import com.example.atheneum.viewmodels.UserViewModel;
-import com.example.atheneum.viewmodels.UserViewModelFactory;
 import com.google.firebase.auth.FirebaseUser;
-
-import java.util.ArrayList;
-
 
 /**
  * The activity for users to view their own profiles
@@ -51,19 +28,6 @@ public class ViewProfileActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_profile);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        final NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
-
-        View headerView = navigationView.getHeaderView(0);
-//        final TextView nav_username = (TextView) ((View) headerView).findViewById(R.id.nav_user_name);
 
         Intent intent = getIntent();
         User user = (User) intent.getSerializableExtra("user");
@@ -116,32 +80,6 @@ public class ViewProfileActivity extends AppCompatActivity {
 
                 }
             });
-        }
-
-        // Update user information in the navbar
-        if (FirebaseAuthUtils.isCurrentUserAuthenticated()) {
-            FirebaseUser firebaseUser = FirebaseAuthUtils.getCurrentUser();
-            UserViewModelFactory userViewModelFactory = new UserViewModelFactory(firebaseUser.getUid());
-            UserViewModel userViewModel = ViewModelProviders.of(this, userViewModelFactory).get(UserViewModel.class);
-            LiveData<User> userLiveData = userViewModel.getUserLiveData();
-            userLiveData.observe(this, new Observer<User>() {
-                @Override
-                public void onChanged(@Nullable User user) {
-                    Log.i(TAG, "in Observer!");
-                    if (user != null) {
-//                        nav_username.setText(user.getUserName());
-
-                        ArrayList<String> photos = user.getPhotos();
-                        if (!photos.isEmpty()) {
-                            Bitmap profilePic = PhotoUtils.DecodeBase64BitmapPhoto(photos.get(0));
-                            ImageView imageView = (ImageView) ((View) navigationView).findViewById(R.id.nav_user_profile_picture);
-                            imageView.setImageBitmap(profilePic);
-                        }
-                    }
-                }
-            });
-        } else {
-            Log.w(TAG, "Shouldn't happen!");
         }
     }
 
