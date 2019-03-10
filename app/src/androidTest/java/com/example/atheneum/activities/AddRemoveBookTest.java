caf468e9a21d87750b8576450d233488134112df
Alex Chen <yin6@ualberta.ca>
2019-03-09 21:03:12 -0700

    created new file for adding and removing book tests

---
+++ app/src/androidTest/java/com/example/atheneum/activities/AddRemoveBookTest.java
@@ -0,0 +1,25 @@
+package com.example.atheneum.activities;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+@RunWith(AndroidJUnit4.class)
+public class AddRemoveBookTest {
+    @Before
+    public void login() {
+        // Login UI emulation taken from FirebaseUIAuthActivitySignInTest.java
+
+
+    }
+
+    @After
+    public void logout() {
+        // Taken from FirebaseUIAuthActivitySignInTest.java
+        
+    }
+
+}
