5e33c57dc74233a0ed87b9f0f1163cb68d8ddf13
Kaho-Lee <ljh243771593@hotmail.com>
2019-03-18 14:41:58 -0600

    change book status when make request

--- app/src/main/java/com/example/atheneum/fragments/BorrowerPageFragment.java
+++ app/src/main/java/com/example/atheneum/fragments/BorrowerPageFragment.java
@@ -161,6 +161,7 @@ public class BorrowerPageFragment extends Fragment {
 
                                 }
                                 requestAdapter.notifyDataSetChanged();
+//                                requestList.clear();
 
                             }
                         }
