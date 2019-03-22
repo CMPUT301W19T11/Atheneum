5fb8875ea8eb8f9310dcc382e8a5d6829c85fd97
Marcus Boay <marcusboay@hotmail.com>
2019-03-22 15:38:11 -0600

    init notifications activity

---
+++ app/src/main/java/com/example/atheneum/activities/NotificationsActivity.java
@@ -0,0 +1,23 @@
+package com.example.atheneum.activities;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import com.example.atheneum.R;
+
+public class NotificationsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notifications);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+}
