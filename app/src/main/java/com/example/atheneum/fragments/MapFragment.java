032b29dd8e51e7c51f3073e1e38cc1832d67c92f
samjwu <swm1000@hotmail.com>
2019-03-27 14:40:40 -0600

    set map type to normal

--- app/src/main/java/com/example/atheneum/fragments/MapFragment.java
+++ app/src/main/java/com/example/atheneum/fragments/MapFragment.java
@@ -139,18 +139,12 @@ public class MapFragment extends Fragment implements OnMapReadyCallback {
                 != PackageManager.PERMISSION_GRANTED
                 && ActivityCompat.checkSelfPermission(getActivity(), Manifest.permission.ACCESS_COARSE_LOCATION)
                 != PackageManager.PERMISSION_GRANTED) {
-            // TODO: Consider calling
-            //    ActivityCompat#requestPermissions
-            // here to request the missing permissions, and then overriding
-            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-            //                                          int[] grantResults)
-            // to handle the case where the user grants the permission. See the documentation
-            // for ActivityCompat#requestPermissions for more details.
             return;
         }
         map.setMyLocationEnabled(true);
         getDeviceLocation();
         googleMap = map;
+        googleMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
     }
 
     @Override
