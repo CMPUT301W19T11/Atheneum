086f6e15e30dfb89896057c22b0678fa02f5039e
Alex Chen <yin6@ualberta.ca>
2019-03-28 20:26:48 -0600

    Change notifications to use BookInfoActivity, untested

--- app/src/main/java/com/example/atheneum/utils/NotificationIntentProvider.java
+++ app/src/main/java/com/example/atheneum/utils/NotificationIntentProvider.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 
 import com.example.atheneum.activities.BookInfoActivity;
 import com.example.atheneum.activities.ShowRequestInfoActivity;
+import com.example.atheneum.models.Book;
 import com.example.atheneum.models.Notification;
 
 public class NotificationIntentProvider {
@@ -13,15 +14,17 @@ public class NotificationIntentProvider {
         if (notification.getrNotificationType() == Notification.NotificationType.REQUEST) {
             // this notification is received by owner
             notifyIntent = new Intent(c, BookInfoActivity.class);
+            notifyIntent.putExtra(BookInfoActivity.VIEW_TYPE, BookInfoActivity.OWNER_VIEW);
         } else {
             // this notification is received by borrower
             notifyIntent = new Intent(c, ShowRequestInfoActivity.class);
+            notifyIntent.putExtra(BookInfoActivity.VIEW_TYPE, BookInfoActivity.REQUSET_VIEW);
         }
 
-        notifyIntent.putExtra(ShowRequestInfoActivity.BOOK_ID, notification.getBookID());
+        notifyIntent.putExtra(BookInfoActivity.BOOK_ID, notification.getBookID());
         // TODO: rStatus in ShowRequestInfoActivity should be obtained within itself
         // BELOW IS PLACEHOLDER AND SHOULD BE THE STATUS OF THE REQUEST INSTEAD OF THE NOTIFICATION
-        notifyIntent.putExtra(ShowRequestInfoActivity.RSTATUS, notification.getrNotificationType().toString());
+//        notifyIntent.putExtra(ShowRequestInfoActivity.RSTATUS, notification.getrNotificationType().toString());
 
         return notifyIntent;
     }
