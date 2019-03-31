aebe5e630742b9466a41154cd567f4ed1d483a19
Alex Chen <yin6@ualberta.ca>
2019-03-30 20:15:06 -0600

    removed commented code

--- app/src/main/java/com/example/atheneum/fragments/BorrowerRequestsFragment.java
+++ app/src/main/java/com/example/atheneum/fragments/BorrowerRequestsFragment.java
@@ -174,35 +174,7 @@ public class BorrowerRequestsFragment extends Fragment {
 
             }
         });
-
-//<<<<<<< HEAD
-//=======
-//
-//        RequestAdapter = new RequestAdapter(BorrowerRequestsFragment.this.context, R.layout.request_list_item, requestList);
-//        requestView.setAdapter(RequestAdapter);
-//
-//        requestView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-//            @Override
-//            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-//                Pair listItemPair = (Pair) requestView.getItemAtPosition(position);
-//                Book listItem = (Book) listItemPair.first;
-//
-//                requestInfoIndent.putExtra("bookID", listItem.getBookID());
-//
-//                Log.d(TAG, "find requested book1 " + listItem.getBookID());
-//                requestInfoIndent.putExtra("rStatus", (String) listItemPair.second);
-//                startActivity(requestInfoIndent);
-//
-//
-//            }
-//        });
-////        requestList.clear();
-////        RequestAdapter.notifyDataSetChanged();
-//
-//
-//
-//
-//>>>>>>> master
+        
         /**
          * go to request generation activity
          */
@@ -221,88 +193,4 @@ public class BorrowerRequestsFragment extends Fragment {
 
         return this.view;
     }
-//<<<<<<< HEAD
-//=======
-//
-//    public void retriveRequest(){
-//        /**
-//         * Retrieve request list
-//         */
-//
-//
-//        /**
-//         * Get the request list
-//         */
-//        if(this.view !=null && this.view.getGlobalVisibleRect(new Rect())) {
-//
-//            final FirebaseDatabase db = FirebaseDatabase.getInstance();
-//            final DatabaseReference ref = db.getReference().child(getString(R.string.db_requestCollection))
-//                    .child(currentUser.getUid());
-//
-//
-//            Log.d(TAG, "Current visibility is "+ String.valueOf(this.view.getGlobalVisibleRect(new Rect())));
-//            ref.addListenerForSingleValueEvent(new ValueEventListener() {
-//
-//                @Override
-//                public void onDataChange(DataSnapshot dataSnapshot) {
-//                    Log.d(TAG, "Current visibility 1 is "+ String.valueOf(view.getGlobalVisibleRect(new Rect())));
-//                    requestList.clear();
-//
-//                    for (DataSnapshot item : dataSnapshot.getChildren()) {
-//
-//
-//                        String bookID = item.child(getString(R.string.db_book_bookID)).getValue(String.class);
-//                        final String Status = item.child(getString(R.string.db_book_request_status)).getValue(String.class);
-//
-//                        DatabaseReference ref_book = db.getReference().child("books").child(bookID);
-//                        ref_book.addListenerForSingleValueEvent(new ValueEventListener() {
-//                            @Override
-//                            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-//                                if (dataSnapshot.exists()) {
-//
-//                                    book = dataSnapshot.getValue(Book.class);
-//                                    Log.d(TAG, "find book " + book.getTitle());
-//                                    Log.d(TAG, "find book with rStatus " + rStatus);
-//                                    Pair newPair = (new Pair(book, Status));
-//                                    if (!requestList.contains(newPair)) {
-//                                        if (rStatus.equals("ALL")) {
-//                                            requestList.add(newPair);
-//
-//
-//                                        } else if (rStatus.equals(Status)) {
-//                                            requestList.add(newPair);
-//                                        }
-//                                    }
-//                                    RequestAdapter.notifyDataSetChanged();
-//                                    requestView.setAdapter(RequestAdapter);
-//                                    ref.removeEventListener(this);
-//
-////                                requestList.clear();
-//
-//                                }
-//                            }
-//
-//                            @Override
-//                            public void onCancelled(@NonNull DatabaseError databaseError) {
-//
-//                            }
-//                        });
-//
-//                    }
-//                }
-//
-//                @Override
-//                public void onCancelled(DatabaseError databaseError) {
-//                }
-//            });
-//
-//        }
-//
-//
-//        Log.d(TAG, "find request size of list "+Integer.toString(requestList.size()));
-//
-//    }
-//
-//
-//>>>>>>> master
 }
